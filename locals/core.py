e8f52e0739f93e60a205ade4069fcf2efe450027
Joe Filippazzo <jfilippazzo@stsci.edu>
2018-02-12 15:07:06 -0500

    Added text function.

--- locals/core.py
+++ locals/core.py
@@ -11,10 +11,25 @@ Low-mass Object Characterization by AnaLyzing Slitless Spectroscopy (LOCALS) is 
 import os
 import numpy as np
 import matplotlib.pyplot as plt
-from SEDkit import seds
+from SEDkit import sed
 from astrodbkit import astrodb, astrocat
 from astropy.io import fits
+import astropy.table as at
 import glob
+
+def test():
+    """
+    A function to test SourceCatalog creation
+    """
+    
+    # Initialize empty source catclog
+    cat = SourceCatalog('/grp/jwst/ssb/test_build7.1/examples_for_dms/level2/nrc_grism/')
+    
+    # Ingest the source data
+    x1d_file = glob.glob(os.path.join(cat.dirpath,'*_x1d.fits'))[0]
+    cat.ingest_sources(x1d_file)
+    
+    return cat
 
 class Source(object):
     """
@@ -31,7 +46,7 @@ class SourceCatalog(object):
     A class to ingest a JWST pipeline output to produce a source catalog
     """
     
-    def __init__(self, dirpath='/grp/jwst/ssb/test_build7.1/examples_for_dms/level2/nrc_grism/'):
+    def __init__(self, dirpath):
         """
         Initialize the SourceCatalog object
         
@@ -47,10 +62,10 @@ class SourceCatalog(object):
         self.cat_file = glob.glob(os.path.join(self.dirpath,'*.ecsv'))[0]
         self.source_list = at.Table.read(self.cat_file, format='ascii.ecsv')
         
-        # Ingest the source data
-        _ingest_sources()
+        self.sources = []
+        self.x1d_files = []
         
-    def _ingest_sources(self):
+    def ingest_sources(self, x1d_file):
         """
         Ingest the FITS files in the given directory and parse
         """
@@ -59,25 +74,28 @@ class SourceCatalog(object):
         # self.cal_file = glob.glob(os.path.join(self.dirpath,'*_cal.fits'))[0]
         
         # Get the 1D extracted spectra (_x1d.fits)
-        self.x1d_file = glob.glob(os.path.join(self.dirpath,'*_x1d.fits'))[0]
+        self.x1d_files.append(x1d_file)
         
-        cal_hdu = fits.open(self.x1d_file)
+        x1d_hdu = fits.open(x1d_file)
         for n in range(len(x1d_hdu)):
+
+            # Initialize source object
+            source = Source()
+
+            # Add the FITS data to the source object
             if x1d_hdu[n].name=='EXTRACT1D':
-                
-                source = Source()
-                
+
                 # Parse the data
                 for col in x1d_hdu[n].data.dtype.names:
                     setattr(source, col, x1d_hdu[n].data[col])
-                    
+
                 # Parse the header
                 for card in x1d_hdu[n].header.cards:
                     setattr(source, card[0], card[1])
-                
-                self.sources[n] = source
-        
-        cal_hdu.close()
+
+                self.sources.append(source)
+
+        x1d_hdu.close()
             
     def xmatch_sources(self, radius=0.00001):
         """
