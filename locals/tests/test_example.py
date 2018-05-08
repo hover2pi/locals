339ec2b824e8e5583f4a0d9dce7066001b558a0c
Joe Filippazzo <jfilippazzo@stsci.edu>
2018-05-08 16:13:12 -0400

    Added data and got notebook working.

--- locals/tests/test_example.py
+++ locals/tests/test_example.py
@@ -1,18 +1,42 @@
-
-def test_primes_c():
-    from ..example_c import primes as primes_c
-    assert primes_c(10) == [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
-
-
-
 def test_primes():
     from ..example_mod import primes
     assert primes(10) == [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
 
 
-def test_deprecation():
-    import warnings
-    warnings.warn(
-        "This is deprecated, but shouldn't raise an exception, unless "
-        "enable_deprecations_as_exceptions() called from conftest.py",
-        DeprecationWarning)
+def testSourceCatalog():
+    """
+    Test to see if SourceCatalog can be assembled
+    """
+    # Cat path
+    cat_path = '/Users/jfilippazzo/Desktop/LOCALS_data/sources/'
+    
+    # Initialize empty source catalog
+    cat = SourceCatalog(cat_path)
+    
+    return cat
+    
+def testSource():
+    """
+    Test to see that Vega source can be assembled
+    """
+    ra = 279.23473479*q.deg
+    dec = 38.78368896*q.deg
+    
+    # Make source
+    source = src.Source(ra=ra, dec=dec, name='Vega')
+    
+    # Find photometry
+    source.find_photometry()
+    
+    return source
+    
+def testSED():
+    
+    # Run test catalog and grab Vega
+    cat = testSourceCatalog()
+    source = cat.sources[1]
+    
+    # Pass data to MakeSED
+    db = astrodb.Database('/Users/jfilippazzo/Documents/Modules/BDNYCdevdb/bdnycdev.db')
+    s = sed.MakeSED(source.id, db, from_dict=source)
+    s.plot()
