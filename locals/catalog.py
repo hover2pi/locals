286bb0e8b725622f02266b957c956c5b0a794100
Joe Filippazzo <jfilippazzo@stsci.edu>
2018-05-09 13:15:46 -0400

    Added dummy data and function to generate it.

--- locals/catalog.py
+++ locals/catalog.py
@@ -45,10 +45,10 @@ class SourceCatalog(object):
         self.source_list = at.Table.read(self.cat_file, format='ascii.ecsv')
         self.sources = []
         self.source_ids = []
-        self.x1d_files = glob.glob(os.path.join(self.dirpath,'*_x1d.fits'))
+        self.x1d_file = glob.glob(os.path.join(self.dirpath,'*_x1d.fits'))[0]
         
         # Make a Source object for each row in the source_list
-        for row in self.source_list:
+        for n,row in enumerate(self.source_list):
             ra = row['icrs_centroid'].ra
             dec = row['icrs_centroid'].dec
             source = Source(ra=ra, dec=dec, **{k:row[k] for k in row.colnames})
@@ -59,95 +59,39 @@ class SourceCatalog(object):
             # Look for distance
             source.find_parallax()
             
+            # Add observed JWST photometry to the source
+            # TODO
+            
+            # Add observed WFSS spectrum to the source
+            wave_units = q.um
+            flux_units = q.erg/q.s/q.cm**2/q.AA
+            source.add_spectrum_file(self.x1d_file, wave_units, flux_units, ext=('EXTRACT1D', n+1))
+            
+            # Add the source to the catalog
             self.source_ids.append(int(source.id))
             self.sources.append(source)
-            
-        # Ping Julia and Kevin to see if there are aperture phot results with WFSS output to do color cuts here
-        
-        # Generate JWST colors for BDs 
-            
-        # Open up x1d files and add spectra to the sources
-        # for x1d_file in self.x1d_files:
-        #
-        #     x1d_hdu = fits.open(x1d_file)
-        #     for n in range(len(x1d_hdu)):
-        #
-        #         # Add the FITS data to the source object
-        #         if x1d_hdu[n].name=='EXTRACT1D':
-        #
-        #             # Get the source_id for this spectrum
-        #             source_id = int(x1d_hdu[n].header['SOURCEID'])
-        #             source_idx = self.source_ids.index(source_id)
-        #             source = self.sources[source_idx]
-        #
-        #             # Put in dummy data
-        #             if dummy:
-        #                 fake_data = np.genfromtxt(pkg_resources.resource_filename('locals', 'data/STSci_Vega.txt'), unpack=True)
-        #                 w = fake_data[0]*q.um
-        #                 f = fake_data[1]*q.erg/q.s/q.cm**2/q.AA
-        #
-        #             source.add_spectrum(w, f)
-        #
-        #             self.sources[source_idx] = source
-        #
-        #     x1d_hdu.close()
-        
-    # def ingest_sources(self, x1d_file, dummy=True):
-    #     """
-    #     Ingest the FITS files in the given directory and parse
-    #
-    #     Parameters
-    #     ----------
-    #     x1d_file: str
-    #         The path to the Level 2 x1d file
-    #     dummy: bool
-    #         Add dummy flux from file
-    #     """
-    #     # Get the calibrated 2D spectroscopic data (_cal.fits)
-    #     # DO WE NEED THIS?
-    #     # self.cal_file = glob.glob(os.path.join(self.dirpath,'*_cal.fits'))[0]
-    #
-    #     # Get the 1D extracted spectra (_x1d.fits)
-    #     self.x1d_files.append(x1d_file)
-    #
-    #     x1d_hdu = fits.open(x1d_file)
-    #     for n in range(len(x1d_hdu)):
-    #
-    #         # Initialize source object
-    #         source = Source()
-    #
-    #         # Add the data from the source_list
-    #         # for col in self.source_list.colnames:
-    #         #     print(self.source_list.loc['id'])
-    #             # setattr(self, col, self.source_list[col][n])
-    #
-    #         # Add the FITS data to the source object
-    #         if x1d_hdu[n].name=='EXTRACT1D':
-    #
-    #             # Parse the data
-    #             for col in x1d_hdu[n].data.dtype.names:
-    #                 setattr(source, col, x1d_hdu[n].data[col])
-    #
-    #             # Parse the header
-    #             for card in x1d_hdu[n].header.cards:
-    #                 setattr(source, card[0], card[1])
-    #
-    #             # Put in dummy data
-    #             if dummy:
-    #                 fake_data = np.genfromtxt(pkg_resources.resource_filename('locals', 'data/STSci_Vega.txt'), unpack=True)
-    #                 source.FLUX = np.interp(source.WAVELENGTH, fake_data[0]/10000., fake_data[1])
-    #
-    #             self.sources.append(source)
-    #
-    #     x1d_hdu.close()
-    #
-    # def xmatch_sources(self, radius=10*q.arcsec, catalogs=['II/246/out','II/328/allwise','V/147/sdss12','II/243/denis']):
-    #     """
-    #     Run astroquery to get ancillary data for each source and add to it's Source instance
-    #     """
-    #     for source in self.sources:
-    #
-    #         # Get the ICRS coordinates from the table and perform Vizier query
-    #         result = Vizier.query_region(source['icrs_centroid'], radius=radius, catalog=catalogs)
-    #
-    #         self.sources[n].raw_data = result
+
+
+def make_dummy_data(x1d_file):
+    """Put real spectra into the x1d file for testing"""
+    import copy
+    
+    hdu = fits.open(x1d_file)
+    
+    # LHS 2924
+    dummy_file = '/Users/jfilippazzo/Dropbox/BDNYC_spectra/SpeX/IRTF Library (Prism+LXD)/M9V_LHS2924.fits'
+    dummy_data = fits.getdata(dummy_file)
+    dummy_rec = np.rec.array(list(zip(*dummy_data)), formats='float32,float32,float32', names='WAVELENGTH,FLUX,ERROR')
+    hdu[2].data = dummy_rec
+    
+    # APMP 0559 (subdwarf!)
+    dummy_file = '/Users/jfilippazzo/Dropbox/BDNYC_spectra/SpeX/Prism/! Subdwarf spectra that need source_ids (not in db yet!)/spex-prism_APMPM0559-2903_20051231_BUR06A.txt'
+    dummy_data = np.genfromtxt(dummy_file, unpack=True)
+    dummy_rec = np.rec.array(list(zip(*dummy_data)), formats='float32,float32,float32', names='WAVELENGTH,FLUX,ERROR')
+    hdu[1].data = dummy_rec
+    
+    # Write the new file
+    hdu.writeto(x1d_file.replace('_x1d','_dummy_x1d'), overwrite=True)
+    
+    hdu.close()
+    
